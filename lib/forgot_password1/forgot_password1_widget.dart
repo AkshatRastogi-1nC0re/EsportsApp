c881adfa194ecc534bd949fab3b6bf5f68fb6e20
Rishika Solanki <37494973+Aryan-Solanki@users.noreply.github.com>
2021-10-15 00:14:08 +0530

    ..

--- lib/forgot_password1/forgot_password1_widget.dart
+++ lib/forgot_password1/forgot_password1_widget.dart
@@ -28,174 +28,181 @@ class _ForgotPassword1WidgetState extends State<ForgotPassword1Widget> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      key: scaffoldKey,
-      backgroundColor: Color(0xFFF5F5F5),
-      body: Stack(
-        children: [
-          Stack(
-            children: [
-              Stack(
-                children: [
-                  Align(
-                    alignment: AlignmentDirectional(0, 0),
-                    child: Image.asset(
-                      'assets/images/BG.png',
-                      width: double.infinity,
-                      height: double.infinity,
-                      fit: BoxFit.fill,
-                    ),
-                  ),
-                  Align(
-                    alignment: AlignmentDirectional(0, -0.55),
-                    child: Image.asset(
-                      'assets/images/esports__cult_final_logo_1.png',
-                      width: MediaQuery.of(context).size.width * 0.6,
-                      height: MediaQuery.of(context).size.height * 0.2,
-                      fit: BoxFit.fill,
-                    ),
-                  ),
-                  Align(
-                    alignment: AlignmentDirectional(0, 0.35),
-                    child: Padding(
-                      padding: EdgeInsetsDirectional.fromSTEB(5, 10, 0, 10),
-                      child: FFButtonWidget(
-                        onPressed: () async {
-                          setState(() => _loadingButton = true);
-                          try {
-                            await Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => OtpsentWidget(),
-                              ),
-                            );
-                          } finally {
-                            setState(() => _loadingButton = false);
-                          }
-                        },
-                        text: 'Get OTP',
-                        options: FFButtonOptions(
-                          width: 260,
-                          height: 50,
-                          color: Color(0x003474E0),
-                          textStyle: FlutterFlowTheme.subtitle2.override(
-                            fontFamily: 'Poppins',
-                            color: Colors.white,
-                            fontSize: 20,
+    return SafeArea(
+      child: Scaffold(
+        key: scaffoldKey,
+        backgroundColor: Color(0xFFF5F5F5),
+        body: SingleChildScrollView( 
+          child: Container(
+            height: MediaQuery.of(context).size.height,
+            child: Stack(
+              children: [
+                Stack(
+                  children: [
+                    Stack(
+                      children: [
+                        Align(
+                          alignment: AlignmentDirectional(0, 0),
+                          child: Image.asset(
+                            'assets/images/BG.png',
+                            width: double.infinity,
+                            height: double.infinity,
+                            fit: BoxFit.fill,
                           ),
-                          borderSide: BorderSide(
-                            color: Color(0xFFFE5082),
-                            width: 2,
-                          ),
-                          borderRadius: 12,
                         ),
-                        loading: _loadingButton,
+                        Align(
+                          alignment: AlignmentDirectional(0, -0.55),
+                          child: Image.asset(
+                            'assets/images/esports__cult_final_logo_1.png',
+                            width: MediaQuery.of(context).size.width * 0.6,
+                            height: MediaQuery.of(context).size.height * 0.2,
+                            fit: BoxFit.fill,
+                          ),
+                        ),
+                        Align(
+                          alignment: AlignmentDirectional(0, 0.35),
+                          child: Padding(
+                            padding: EdgeInsetsDirectional.fromSTEB(5, 10, 0, 10),
+                            child: FFButtonWidget(
+                              onPressed: () async {
+                                setState(() => _loadingButton = true);
+                                try {
+                                  await Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => OtpsentWidget(),
+                                    ),
+                                  );
+                                } finally {
+                                  setState(() => _loadingButton = false);
+                                }
+                              },
+                              text: 'Get OTP',
+                              options: FFButtonOptions(
+                                width: 260,
+                                height: 50,
+                                color: Color(0x003474E0),
+                                textStyle: FlutterFlowTheme.subtitle2.override(
+                                  fontFamily: 'Poppins',
+                                  color: Colors.white,
+                                  fontSize: 20,
+                                ),
+                                borderSide: BorderSide(
+                                  color: Color(0xFFFE5082),
+                                  width: 2,
+                                ),
+                                borderRadius: 12,
+                              ),
+                              loading: _loadingButton,
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                    Align(
+                      alignment: AlignmentDirectional(-0.02, -0.17),
+                      child: AutoSizeText(
+                        'Reset your password',
+                        textAlign: TextAlign.center,
+                        style: FlutterFlowTheme.bodyText1.override(
+                          fontFamily: 'Poppins',
+                          color: Colors.white,
+                          fontSize: 20,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+                Align(
+                  alignment: AlignmentDirectional(0, 0.05),
+                  child: Padding(
+                    padding: EdgeInsetsDirectional.fromSTEB(30, 0, 30, 0),
+                    child: TextFormField(
+                      controller: textController,
+                      obscureText: false,
+                      decoration: InputDecoration(
+                        hintText: '+91 956*******',
+                        hintStyle: FlutterFlowTheme.bodyText1.override(
+                          fontFamily: 'Poppins',
+                          color: Colors.white,
+                        ),
+                        enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Color(0x00000000),
+                            width: 1,
+                          ),
+                          borderRadius: const BorderRadius.only(
+                            topLeft: Radius.circular(4.0),
+                            topRight: Radius.circular(4.0),
+                          ),
+                        ),
+                        focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Color(0x00000000),
+                            width: 1,
+                          ),
+                          borderRadius: const BorderRadius.only(
+                            topLeft: Radius.circular(4.0),
+                            topRight: Radius.circular(4.0),
+                          ),
+                        ),
+                        filled: true,
+                        fillColor: Color(0xFF032170),
+                        prefixIcon: Icon(
+                          Icons.local_phone_sharp,
+                          color: Colors.white,
+                          size: 30,
+                        ),
+                      ),
+                      style: FlutterFlowTheme.bodyText1.override(
+                        fontFamily: 'Poppins',
+                        color: Colors.white,
+                      ),
+                      textAlign: TextAlign.center,
+                      maxLines: 1,
+                      keyboardType: TextInputType.phone,
+                    ),
+                  ),
+                ),
+                Align(
+                  alignment: AlignmentDirectional(-0.25, 0.95),
+                  child: AutoSizeText(
+                    'Aready a member ?',
+                    textAlign: TextAlign.center,
+                    style: FlutterFlowTheme.bodyText1.override(
+                      fontFamily: 'Poppins',
+                      color: Colors.white,
+                      fontSize: 10,
+                    ),
+                  ),
+                ),
+                Align(
+                  alignment: AlignmentDirectional(0.3, 0.95),
+                  child: InkWell(
+                    onTap: () async {
+                      await Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => SignInPageWidget(),
+                        ),
+                      );
+                    },
+                    child: AutoSizeText(
+                      'Log in',
+                      textAlign: TextAlign.center,
+                      style: FlutterFlowTheme.bodyText1.override(
+                        fontFamily: 'Poppins',
+                        color: Colors.white,
+                        fontSize: 10,
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
-                  )
-                ],
-              ),
-              Align(
-                alignment: AlignmentDirectional(-0.02, -0.17),
-                child: AutoSizeText(
-                  'Reset your password',
-                  textAlign: TextAlign.center,
-                  style: FlutterFlowTheme.bodyText1.override(
-                    fontFamily: 'Poppins',
-                    color: Colors.white,
-                    fontSize: 20,
                   ),
-                ),
-              )
-            ],
-          ),
-          Align(
-            alignment: AlignmentDirectional(0, 0.05),
-            child: Padding(
-              padding: EdgeInsetsDirectional.fromSTEB(30, 0, 30, 0),
-              child: TextFormField(
-                controller: textController,
-                obscureText: false,
-                decoration: InputDecoration(
-                  hintText: '+91 956*******',
-                  hintStyle: FlutterFlowTheme.bodyText1.override(
-                    fontFamily: 'Poppins',
-                    color: Colors.white,
-                  ),
-                  enabledBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Color(0x00000000),
-                      width: 1,
-                    ),
-                    borderRadius: const BorderRadius.only(
-                      topLeft: Radius.circular(4.0),
-                      topRight: Radius.circular(4.0),
-                    ),
-                  ),
-                  focusedBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Color(0x00000000),
-                      width: 1,
-                    ),
-                    borderRadius: const BorderRadius.only(
-                      topLeft: Radius.circular(4.0),
-                      topRight: Radius.circular(4.0),
-                    ),
-                  ),
-                  filled: true,
-                  fillColor: Color(0xFF032170),
-                  prefixIcon: Icon(
-                    Icons.local_phone_sharp,
-                    color: Colors.white,
-                    size: 30,
-                  ),
-                ),
-                style: FlutterFlowTheme.bodyText1.override(
-                  fontFamily: 'Poppins',
-                  color: Colors.white,
-                ),
-                textAlign: TextAlign.center,
-                maxLines: 1,
-                keyboardType: TextInputType.phone,
-              ),
+                )
+              ],
             ),
           ),
-          Align(
-            alignment: AlignmentDirectional(-0.25, 0.95),
-            child: AutoSizeText(
-              'Aready a member ?',
-              textAlign: TextAlign.center,
-              style: FlutterFlowTheme.bodyText1.override(
-                fontFamily: 'Poppins',
-                color: Colors.white,
-                fontSize: 10,
-              ),
-            ),
-          ),
-          Align(
-            alignment: AlignmentDirectional(0.3, 0.95),
-            child: InkWell(
-              onTap: () async {
-                await Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => SignInPageWidget(),
-                  ),
-                );
-              },
-              child: AutoSizeText(
-                'Log in',
-                textAlign: TextAlign.center,
-                style: FlutterFlowTheme.bodyText1.override(
-                  fontFamily: 'Poppins',
-                  color: Colors.white,
-                  fontSize: 10,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-          )
-        ],
+        ),
       ),
     );
   }
